efd40d503d8eec6fb7264b9893b1171ef7ddc69f
Myles Horton <myles@getlantern.org>
2017-10-05 12:11:39 -0700

    updated with new tag and short name

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
 	console.log('in extension')
         if( details.url == "https://www.amazon.com/" )
-            return {redirectUrl: "https://www.amazon.com/?tag=thegarcoopenu-20" };
+            return {redirectUrl: "https://www.amazon.com/?tag=gardencoopftw-20" };
     },
     {urls: ["https://www.amazon.com/*"]},
     ["blocking"]);
