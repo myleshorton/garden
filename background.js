071f2546bfeded528c0b086046e4264052f8050c
Myles Horton <myles@getlantern.org>
2017-10-27 14:34:14 -0700

    direct smile link

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if( details.url == "https://www.amazon.com/" )
-            return {redirectUrl: "https://www.amazon.com/?tag=gardencoopftw-20" };
+            return {redirectUrl: "https://smile.amazon.com/ch/95-4816207" };
     },
     {urls: ["https://www.amazon.com/*"]},
     ["blocking"]);
