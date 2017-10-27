542cbfd2ac4bfa0124ccb4fde220985470123f07
Myles Horton <myles@getlantern.org>
2017-10-27 14:35:28 -0700

    use google short link

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if( details.url == "https://www.amazon.com/" )
-            return {redirectUrl: "https://smile.amazon.com/ch/95-4816207" };
+            return {redirectUrl: "https://goo.gl/1iSMnm" };
     },
     {urls: ["https://www.amazon.com/*"]},
     ["blocking"]);
